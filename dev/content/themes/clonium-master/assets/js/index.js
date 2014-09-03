5dc76598c9bb28ad4fbc415062472e3b853beb96
Avinash <avipar@me.com>
2014-09-03 02:37:09 -0400

    Blog update at 2014-09-03 06:37:09

---
+++ dev/content/themes/clonium-master/assets/js/index.js
@@ -0,0 +1,74 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/* Buster: correct local directory links to not include "index.html" */
+(function($){
+    var absUrlPattern = new RegExp('^(?:[a-z]+:)?//', 'i');
+    $(document).ready(function(){
+        $('a').each(function(){
+            var e = $(this);
+            var href = e.attr('href');
+            if (!absUrlPattern.test(href)) {
+                var new_href = href.replace(/\/index\.html$/, '/');
+                e.attr('href', new_href);
+            }
+        });
+    });
+}(jQuery));
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $(".post-content").fitVids();
+
+        function casperFullImg() {
+            $("img").each( function() {
+                var contentWidth = $(".post-content").outerWidth(); // Width of the content
+                var imageWidth = $(this)[0].naturalWidth; // Original image resolution
+
+                if (imageWidth >= contentWidth) {
+                    $(this).addClass('full-img');
+                } else {
+                    $(this).removeClass('full-img');
+                }
+            });
+        };
+
+        casperFullImg();
+        $(window).smartresize(casperFullImg);
+
+    });
+
+}(jQuery));
+
+(function($,sr){
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
